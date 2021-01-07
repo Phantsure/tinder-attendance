eee97c49317f5982c25a3d88f960baa728e84c9f
Phantsure <phantsure@pop-os.localdomain>
2021-01-07 16:52:22 +0530

    Added Procfile for heroku

--- HOME/settings.py
+++ HOME/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "+#m)5=##t$wb@%n&yiu8flzqh4-+@8*3+34_3a^j^syd69(x5_"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["timder-attendance.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
@@ -132,4 +133,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn HOME.wsgi --logfile - 
